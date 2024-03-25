ca8196429511598392a495a71960d0d6758b6112
Oscar Khowong <42625208+oscarlk@users.noreply.github.com>
2024-03-25 00:44:59 -0700

    modified prepare statements in manage.php

--- manage.php
+++ manage.php
@@ -12,7 +12,7 @@ $username = "eason";
 $action = $_POST["action"];
 $amount = (double) $_POST["amount"];
 
-$stmt = $conn->prepare("SELECT balance FROM bank WHERE name=?");
+$stmt = $conn->prepare("SELECT balance FROM users WHERE name=?");
 $stmt->bind_param("s", $username); 
 $stmt->execute();
 $result = $stmt->get_result();
@@ -22,14 +22,14 @@ $current_balance =(double) $arr['balance'];
 switch ($action) {
     case "deposit":
         $new_balance = $current_balance + $amount;
-        $update_query = "UPDATE bank SET balance='$new_balance' WHERE name='$username'";
+        $update_query = "UPDATE users SET balance='$new_balance' WHERE name='$username'";
         mysqli_query($conn, $update_query);
         break;
 
     case "withdraw":
         if ($amount <= $current_balance) {
             $new_balance = $current_balance - $amount;
-            $update_query = "UPDATE bank SET balance='$new_balance' WHERE name='$username'";
+            $update_query = "UPDATE users SET balance='$new_balance' WHERE name='$username'";
             mysqli_query($conn, $update_query);
 
         } else {
